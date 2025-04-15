471b2228f1c23cfa6ccc6eec74a2d970d4c7e4c7
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-15 21:51:27 +0530

    feat: add SVG assets and button component to homepage
    Add new SVG assets (logo.svg, user-placeholder.svg, placeholder.svg) to the public directory and integrate a button component from the UI library into the homepage. These changes enhance the visual design and user interaction on the homepage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <p className="font-bold text-rose-500">Hello world!</p>
+      <Button variant="destructive">Click me</Button>
     </div>
   );
 }
