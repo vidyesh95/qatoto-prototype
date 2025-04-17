fc6c83ca08ba23254cb1083953cf817dd20ca5ef
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-17 20:44:23 +0530

    style(page): update homepage layout and styling
    Refactor the homepage layout to use a flex container and add styling to the images. The changes improve the visual structure and responsiveness of the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <Image src="/website_logo.svg" width={107} height={40} alt="logo" />
-    </div>
+    <section className="w-full bg-gray-500 flex">
+      <div className="flex pl-5 py-2 bg-amber-800 gap-5">
+        <Image
+          src="/menu_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg"
+          width={40}
+          height={40}
+          alt="menu"
+          className="bg-red-500 p-2 cursor-pointer"
+        />
+        <Image
+          src="/website_logo.svg"
+          className="bg-green-400 self-center cursor-pointer"
+          width={107}
+          height={40}
+          alt="logo"
+        />
+      </div>
+    </section>
   );
 }
