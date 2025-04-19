988c7b9e35f3a643ff1f486df49ebff6a645ae2a
Vidyesh Churi <vidyesh95@gmail.com>
2025-04-19 15:31:03 +0530

    refactor(app): restructure page components and move home page to (home) directory
    Deleted redundant feed-related components and moved the home page content to the (home) directory to improve project organization and maintainability

--- src/app/feed/[videoId]/page.tsx
+++
@@ -1,8 +0,0 @@
-interface PageProps {
-  params: Promise<{ videoId: string }>;
-}
-
-export default async function Page({ params }: PageProps) {
-  const { videoId } = await params;
-  return <div>Video Id : {videoId}</div>;
-}
--- src/app/feed/layout.tsx
+++
@@ -1,9 +0,0 @@
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout = ({ children }: LayoutProps) => {
-  return <div>{children}</div>;
-};
-
-export default Layout;
